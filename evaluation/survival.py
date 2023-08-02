d61b21f8213c4e6b2865c277aac51df59d7b58a8
Ste <drstrupf@gmail.com>
2023-08-02 16:24:33 +0200

    Add all functions relevant for computing time to first progression and survival statistics to the survival module.

--- evaluation/survival.py
+++ evaluation/survival.py
@@ -2,16 +2,168 @@
 
 Lifelines: https://lifelines.readthedocs.io/en/latest/
 
-API references:
-    - Kaplan-Meier: https://lifelines.readthedocs.io/en/latest/fitters/univariate/KaplanMeierFitter.html#module-lifelines.fitters.kaplan_meier_fitter
-    - Weibull: https://lifelines.readthedocs.io/en/latest/fitters/univariate/WeibullFitter.html#module-lifelines.fitters.weibull_fitter
-    - Statistics: https://lifelines.readthedocs.io/en/latest/lifelines.statistics.html#module-lifelines.statistics
-
 """
 
 import numpy as np
 import pandas as pd
 import lifelines
+
+
+def get_time_to_first_progression(
+    follow_up_dataframe,
+    edss_score_column_name,
+    time_column_name,
+    reference_score_column_name="reference_edss_score",
+    first_progression_flag_column_name="is_first_progression",
+    additional_columns_to_drop=[],
+    progression_event_found_column_name="progression",
+    time_to_progression_column_name="time_to_first_progression",
+    reference_for_progression_column_name="reference_for_progression",
+    progression_score_column_name="progression_score",
+    length_of_follow_up_column_name="length_of_follow_up",
+):
+    """Get time to progression for a follow-up with annotated
+    baseline and first progression.
+
+    Args:
+        - follow_up_dataframe: a pandas dataframe with one follow-up, reference, progression
+        - edss_score_column_name: column with EDSS score
+        - time_column_name: column with timestamps
+        - reference_score_column_name: column with the reference score
+        - first_progression_flag_column_name: column with the flag for first progression
+
+    Returns:
+        - df: a dataframe with 1 row and the following columns, in addition to any
+          constant columns (e.g. the follow-up ID):
+            - progression: bool, whether a progression event was found
+            - time_to_first_progression: time to first progression event
+            - reference_for_progression: progression was detected w.r.t. this reference
+            - progression_score: the score at the assessment that counts as progression
+            - length_of_follow_up: length of the follow-up period.
+
+    """
+    # Check data type of timestamp column
+    assert pd.api.types.is_numeric_dtype(
+        follow_up_dataframe[time_column_name]
+    ), "Timestamps must be numeric, e.g. an integer number of days after baseline."
+    assert pd.api.types.is_numeric_dtype(
+        follow_up_dataframe[reference_score_column_name + "_" + time_column_name]
+    ), "Timestamps must be numeric, e.g. an integer number of days after baseline."
+    # Assert that the input data are well ordered
+    assert (
+        follow_up_dataframe[time_column_name].is_monotonic_increasing
+        and follow_up_dataframe[time_column_name].is_unique
+    ), "Input data are not well ordered or contain ambiguous timestamps."
+
+    first_assessment_timestamp = follow_up_dataframe.iloc[0][time_column_name]
+    last_assessment_timestamp = follow_up_dataframe.iloc[-1][time_column_name]
+    length_of_follow_up = last_assessment_timestamp - first_assessment_timestamp
+
+    progress_df = follow_up_dataframe[
+        follow_up_dataframe[first_progression_flag_column_name]
+    ]
+    if len(progress_df) == 0:
+        progression_event_found = False
+        time_to_progression = np.NaN
+        reference_for_progression = np.NaN
+        progression_score = np.NaN
+    else:
+        progression_event_found = True
+        time_to_progression = (
+            progress_df.iloc[0][time_column_name] - first_assessment_timestamp
+        )
+        reference_for_progression = progress_df.iloc[0][reference_score_column_name]
+        progression_score = progress_df.iloc[0][edss_score_column_name]
+
+    # Let's keep all ID columns etc., and just drop the
+    # time-dependent stuff, and optionally some more...
+    columns_to_drop = additional_columns_to_drop + [
+        edss_score_column_name,
+        time_column_name,
+        reference_score_column_name,
+        first_progression_flag_column_name,
+        reference_score_column_name + "_" + time_column_name,
+    ]
+    columns_to_keep = [
+        column
+        for column in follow_up_dataframe.columns
+        if column not in columns_to_drop
+    ]
+    return_df_base = (
+        follow_up_dataframe[columns_to_keep]
+        .drop_duplicates()
+        .reset_index(drop=True)
+        .copy()
+    )
+    # Then we add the time to progression etc.
+    return_df_left = pd.DataFrame(
+        [
+            {
+                progression_event_found_column_name: progression_event_found,
+                time_to_progression_column_name: time_to_progression,
+                reference_for_progression_column_name: reference_for_progression,
+                progression_score_column_name: progression_score,
+                length_of_follow_up_column_name: length_of_follow_up,
+            }
+        ]
+    )
+    if return_df_base.empty:
+        return_df = return_df_left
+    else:
+        return_df = pd.concat([return_df_base, return_df_left], axis=1)
+
+    assert (
+        len(return_df) == 1
+    ), "Ambiguous column values, check if you drop all relevant columns!"
+
+    return return_df
+
+
+def get_lifelines_input_data(
+    first_progression_flag,
+    time_to_first_progression,
+    length_of_follow_up,
+    global_censoring=None,
+    duration_name="duration",
+    observed_name="observed",
+):
+    """Format data for use with the lifelines library.
+
+    We use lifelines (https://lifelines.readthedocs.io/en/latest/)
+    for survival analyses. Most fitters (e.g. Kaplan-Meier) require
+    the data points 'duration' (time to event or censoring) and
+    'observed' (bool, whether an event was observed). We add these
+    columns to the dataframe obtained using get_time_to_first_progression.
+
+    Args:
+        - first_progression_flag: bool, whether a progression was observed
+        - time_to_first_progression: time to first progression
+        - length_of_follow_up: duration of follow-up
+        - global_censoring: impose a global cutoff, default is None
+        - duration_name: name of the dict entry for duration
+        - observed_name: name of the dict entry for observed
+
+    Returns:
+        - dict: a dictionary with 'duration' and 'observed'
+
+    """
+    if global_censoring is None:
+        observed = first_progression_flag
+        if time_to_first_progression > 0:
+            duration = time_to_first_progression
+        else:
+            duration = length_of_follow_up
+    else:
+        if (time_to_first_progression > 0) and (
+            time_to_first_progression <= global_censoring
+        ):
+            duration = time_to_first_progression
+            observed = True
+        else:
+            duration = global_censoring
+            observed = False
+
+    return {duration_name: duration, observed_name: observed}
 
 
 def get_median_survival_time_kaplan_meier(
